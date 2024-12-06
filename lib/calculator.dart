06db7d5960a274ec1803b806ed83ab56a247920e
vengersmoon <vengersmon003@gmail.com>
2024-12-06 11:18:18 +0530

    Calculator

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 
-class Calculator extends StatelessWidget {
+class Calculator extends StatefulWidget {
   const Calculator({super.key});
 
   @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+
+ String output = "0";
+ String? input = "";
+ String? operator;
+ double? num1;
+ double? num2;
+
+
+ void buttontap(String value){
+     setState(() {
+        if(value == 'C'){
+           
+           output = "0";
+           input = "";
+           
+        }
+     });
+ }
+
+
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+
+      ),
+
+      
+    );
   }
 }
